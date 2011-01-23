4d2ba2be654479b25ebc2f2b0c18d3ba06fdfbcd
Benjamin Bloomfield <bhb123@gmail.com>
2011-01-23 04:16:18 +0000

    Added psalmtone.js

---
+++ psalmtone.js
@@ -0,0 +1,182 @@
+var regexLatin = /((?:s[uú]bs|tr[aá]ns|p[oó]st|[aá]bs|[oó]bs|[eé]x|(?:[cgq]u(?=[aeiouyáéëíóúıæœ])|[bcdfghjklmnprstvwxz])*([aá]u|[ao][eé]?|[eiuyáéëíóúıæœ])(?:[\wáéíóúı]*(?=-)|(?=[bcdgptf][lr])|(?:[bcdfghjklmnpqrstvwxz]+(?=$|[^\wáéíóúı])|[bcdfghjklmnpqrstvwxz](?=[bcdfghjklmnprstvwxz]+))?)))(?:([\*-])|([^\w\sáéíóúı]+(?=\s|$))?)(\s*|$)/gi;var regexLatin = /((?:s[uú]bs|tr[aá]ns|p[oó]st|[aá]bs|[oó]bs|[eé]x|(?:[cgq]u(?=[aeiouyáéëíóúıæœ])|[bcdfghjklmnprstvwxz])*([aá]u|[ao][eé]?|[eiuyáéëíóúıæœ])(?:[\wáéíóúı]*(?=-)|(?=[bcdgptf][lr])|(?:[bcdfghjklmnpqrstvwxz]+(?=$|[^\wáéíóúı])|[bcdfghjklmnpqrstvwxz](?=[bcdfghjklmnprstvwxz]+))?)))(?:([\*-])|([^\w\sáéíóúı]+(?=\s|$))?)(\s*|$)/gi;var regexLatin = /((?:s[uú]bs|tr[aá]ns|p[oó]st|[aá]bs|[oó]bs|[eé]x|(?:[cgq]u(?=[aeiouyáéëíóúıæœ])|[bcdfghjklmnprstvwxz])*([aá]u|[ao][eé]?|[eiuyáéëíóúıæœ])(?:[\wáéíóúı]*(?=-)|(?=[bcdgptf][lr])|(?:[bcdfghjklmnpqrstvwxz]+(?=$|[^\wáéíóúı])|[bcdfghjklmnpqrstvwxz](?=[bcdfghjklmnprstvwxz]+))?)))(?:([\*-])|([^\w\sáéíóúı]+(?=\s|$))?)(\s*|$)/gi;var regexLatin = /((?:s[uú]bs|tr[aá]ns|p[oó]st|[aá]bs|[oó]bs|[eé]x|(?:[cgq]u(?=[aeiouyáéëíóúıæœ])|[bcdfghjklmnprstvwxz])*([aá]u|[ao][eé]?|[eiuyáéëíóúıæœ])(?:[\wáéíóúı]*(?=-)|(?=[bcdgptf][lr])|(?:[bcdfghjklmnpqrstvwxz]+(?=$|[^\wáéíóúı])|[bcdfghjklmnpqrstvwxz](?=[bcdfghjklmnprstvwxz]+))?)))(?:([\*-])|([^\w\sáéíóúı]+(?=\s|$))?)(\s*|$)/gi;
+var regexAccent = /[áéíóúı]/i;
+var regexToneGabc = /(')?(([^\sr]+)(r)?)(?=$|\s)/gi;
+var sym_flex = '†';
+var sym_med = '*';
+var bi_formats = {html: {bold: ["<b>", "</b>"], italic: ["<i>", "</i>"]},
+                  tex: {bold:  ["{\\bf ", "}"], italic:  ["{\\it ", "}"]}
+                 };
+function syllable(match) {
+  return {index: match.index,
+          all: match[0],
+          syl: match[1],
+          vowel: match[2],
+          separator: match[3], // - or *
+          punctuation: match[4]? match[4] : "",
+          space: match[5],
+          accent: match[3] == '*' || regexAccent.test(match[2])
+         };
+}
+function toneGabc(match) {
+  return {index: match.index,
+          all: match[0],
+          accent: match[1] == "'",
+          gabc: "(" + match[2] + ")",
+          gabcClosed: "(" + match[3] + ")",
+          open: match[4] == "r"
+         };
+}
+function onOpen() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  ss.addMenu("Generate Psalm Tone", getTones());
+}
+
+function getTones() {
+  var sheetTones = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Tones");
+  var range = sheetTones.getRange(2, 1, sheetTones.getMaxRows() - 1, 2);
+  var row = range.getValues();
+  var result = [];
+  for(var i in row) {
+    var tone = String(row[i][0]);
+    if(tone.length == 0)
+      break;
+    var end = String(row[i][1]);
+    result.push({name:tone + "." + end, functionName:"doToneRow1"});
+  }
+  return result;
+}
+
+function applyPsalmTone(text,gabc) {
+  var syl = getSyllables(text);
+  var tones = getGabcTones(gabc);
+  var r = [];
+  var si = syl.length - 1;
+  var lastOpen;
+//  var countIntonation = 0;
+//  for(; countIntonation < tones.length; ++countIntonation) {
+//    var tone = tones[countIntonation];
+//    if(tone.open || tone.accent)
+//      break;
+//  }
+  for(var ti = tones.length - 1; ti >= 0; --ti,--si) {
+    var tone = tones[ti];
+    var s = syl[si];
+    if(tone.open) {
+      if(lastOpen && lastOpen.accent) {
+        while(!s.accent) {
+          r.push(s.space);
+          r.push(tone.gabcClosed);
+          r.push(s.syl);
+          --si;
+          s = syl[si];
+        }
+        lastOpen = undefined;
+	  r.push(s.space);
+        r.push(tone.gabcClosed);
+        r.push(s.syl);
+      } else {
+        lastOpen = tone;
+	  ++si;
+      }
+    } else if(tone.accent) {
+      if(lastOpen) {
+        while(!s.accent) {
+          r.push(s.space);
+          r.push(lastOpen.gabcClosed);
+          r.push(s.syl);
+          --si;
+          s = syl[si];
+        }
+        lastOpen = undefined;
+      } else if(!s.accent) {
+        lastOpen = tone;
+      }
+      r.push(s.space);
+      r.push(tone.gabc);
+      r.push(s.syl);
+      //--si;
+    } else {
+      if(lastOpen) {
+        while(si > ti) {
+          r.push(s.space);
+          r.push(lastOpen.gabcClosed);
+          r.push(s.syl);
+          --si;
+          s = syl[si];
+        }
+        lastOpen = undefined;
+      }
+      r.push(s.space);
+      //if(!lastOpen)
+        r.push(tone.gabc);
+      //else r.push(lastOpen.gabcClosed);
+      r.push(s.syl);
+      //--si;
+    }
+  }
+  return r.reverse().join('');
+}
+
+function getGabcTones(gabc) {
+  var tones = [];
+  var match;
+  while(match=regexToneGabc.exec(gabc)) {
+    tones.push(toneGabc(match));
+  }
+  return tones;
+}
+
+function getSyllables(text) {
+  var syl = [];
+  var match;
+  while(match=regexLatin.exec(text)) {
+    syl.push(syllable(match));
+  }
+  return syl;
+}
+
+function addBoldItalic(text,accents,preperatory,sylsAfterBold,format) {
+  if(!sylsAfterBold) sylsAfterBold = 0;
+  var f = bi_formats[format];
+  if(!f) f = bi_formats.html;
+  var syl = getSyllables(text);
+  var doneAccents = 0;
+  var donePrep = 0;
+  var index = text.length;
+  var len;
+  var sylCount = 0;
+  var sylEndIndex = 0;
+  var i=syl.length - 1;
+  for(; i >= 0 && doneAccents < accents; --i) {
+    var s = syl[i];
+    len = s.all.length;
+    index = s.index;
+    if(sylCount < sylsAfterBold) {
+      ++sylCount;
+      if(sylCount == sylsAfterBold) {
+        sylEndIndex = index;
+      }
+      continue;
+    }
+    if(s.accent) {
+      var stext = s.syl;
+      if(sylEndIndex != 0) {
+        len = sylEndIndex - index;
+        stext = text.substr(index,len);
+        sylEndIndex = 0;
+      }
+      text = text.substr(0,index) + f.bold[0] + stext + f.bold[1] + s.punctuation + s.space + text.substr(index + len);
+      ++doneAccents;
+    }
+  }
+  len = index;
+  for(; i >= 0 && donePrep < preperatory; --i) {
+    index -= syl[i].all.length;
+    ++donePrep;
+  }
+  if(donePrep > 0) {
+    len = len - index;
+    text = text.substr(0,index) + f.italic[0] + text.substr(index,len) + f.italic[1] + text.substr(index+len);
+  }
+  return text;
+}
