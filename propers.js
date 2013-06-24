469d8ff75edd3634f8af662efcd73d9e91c6e8b2
Benjamin Bloomfield <bhb123+g@gmail.com>
2013-06-24 12:27:24 -0400

    I had meant for these to get committed last time

--- propers.js
+++ propers.js
@@ -572,7 +572,7 @@ $(function(){
         ++i;
       } else if(firstVerse || asGabc) {
         var result={shortened:false};
-        if(false && introitTone && line[0].match(/†/)) {
+        if(false && introitTone && line[0].match(/†/)) { //TODO: Make this optional perhaps
           var left = line[0].split('†');
           gabc += (italicNote||'') + applyPsalmTone({
             text: left[0].trim(),
