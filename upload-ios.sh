0d89a42be8020422f6eb60c1634dca1529c0ce31
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-04-12 10:00:14 -0400

    Added script to upload iOS version

---
+++ upload-ios.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
