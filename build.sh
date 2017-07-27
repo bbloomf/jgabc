3814e64f01dd9a2f97ec29f9ff025f5a2d2a96ec
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-07-27 07:54:26 -0400

    Removed amazon web app zip

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-rm az-web-app-jgabc.zip
-zip -9 az-web-app-jgabc.zip www/*
 cordova build --release --device
 /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
