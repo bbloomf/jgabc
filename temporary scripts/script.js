57ebe55ebaa6d28d7459a4f6561ce6dbba036fec
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-09-25 14:50:53 -0400

    Added script

--- temporary scripts/script.js
+++ temporary scripts/script.js
@@ -1,23 +1,74 @@
+var fs = require('fs');
 var main={};
-var files = fs.readdirSync('Latin/tempora-2');
-q = function(){
-  for(f in files){
-    if(files[f].match(/-0\.txt$/)){
-      var code = files[f].slice(0,-6);
-      var name = table[code];
-      data = fs.readFileSync('Latin/tempora-2/' + files[f],{encoding:'UTF8'});
-      if(!name) console.info(code);
-      var temp = {};
-      var introitus = /\[Introitus\]((?:\r?\n[^\r\n]+)*)\r?\n\r?\n/.exec(data);
-      var graduale = /\[Graduale\]((?:\r?\n[^\r\n]+)*)\r?\n\r?\n/.exec(data);
-      var offertorium = /\[Offertorium\]((?:\r?\n[^\r\n]+)*)\r?\n\r?\n/.exec(data);
-      var communio = /\[Communio\]((?:\r?\n[^\r\n]+)*)\r?\n\r?\n/.exec(data);
-      if(introitus) temp['introitus'] = introitus[1].trimLeft();
-      if(graduale) temp['graduale'] = graduale[1].trimLeft();
-      if(offertorium) temp['offertorium'] = offertorium[1].trimLeft();
-      if(communio) temp['communio'] = communio[1].trimLeft();
-      main[code] = temp;
+var files = fs.readdirSync('../Psalms-RGP/rgp-pointed');
+var novaVulgata = fs.readFileSync('../psalms/NovaVulgata.txt', {encoding:'UTF8'}).split(/\s*\n\s*/);
+var regexLatin = /((?:<\w+>)*)(((?:(?:(\s+)|)(?:(?:i(?!i)|(?:n[cg]|q)u)(?=[aeiouyáéëíóúýǽæœ])|[bcdfghjklmnprstvwxz]*)([aá]u|[ao][eé]?|[eiuyáéëíóúýǽæœ])(?:[\wáéíóúýǽæœ]*(?=-)|(?=(?:n[cg]u|sc|[sc][tp]r?|gn|ps)[aeiouyáéëíóúýǽæœ]|[bcdgptf][lrh][\wáéíóúýǽæœ])|(?:[bcdfghjklmnpqrstvwxz]+(?=$|[^\wáëéíóúýǽæœ])|[bcdfghjklmnpqrstvwxz](?=[bcdfghjklmnpqrstvwxz]+))?)))(?:([\*-])|([^\w\sáëéíóúýǽæœ]*(?:\s[:;†\*\"«»‘’“”„‟‹›‛])*\.?(?=\s|$))?)(?=(\s*|$)))((?:<\/\w+>)*)/gi;
+var regexAccent = /[áéíóúýǽ]/i;
+var regexCapitalVowel = /[AEIOUYÆŒæœ]/
+
+var regexEnglish = /[a-z']+/gi;
+var regexEnglishAccent = /'/;
+var linesWithThreeOrMoreSyllablesBetweenFinalAccents = 0;
+var linesWithMonoSyllableImmediatelyPrecedingFinalAccent = [];
+q = function(line, reSyl, reAccent){
+  var syllables = line.match(reSyl);
+  if(!syllables) return false;
+  var accents = [];
+  for(var i = syllables.length - 1; i >= 0; --i) {
+    var hasAccent = false;
+    if(regexLatin == reSyl) {
+      hasAccent = syllables[i].match(reAccent);
+      if(!hasAccent && (i == 0 || syllables[i][0] == ' ')) {
+        hasAccent = syllables[i+1] && syllables[i+1][0] != ' ' && (!syllables[i+2] || syllables[i+2][0] == ' ');
+      }
+      if(!hasAccent && syllables[i].match(regexCapitalVowel)) {
+        for(var j = i+1; j < syllables.length; ++j) {
+          if(syllables[j].match(regexAccent)) break;
+          if(j == syllables.length - 1 || syllables[j][0] == ' ') {
+            hasAccent = true;
+            break;
+          }
+        }
+      }
+    }
+    if(hasAccent || syllables[i].match(reAccent)) {
+      accents.push(i);
+      if(accents.length == 2) break;
     }
   }
+  if(syllables.length >= 2 && (accents[0] - accents[1]) >= 4) {
+    ++linesWithThreeOrMoreSyllablesBetweenFinalAccents;
+    //console.info(line);
+  }
+  if(syllables[accents[0]][0] == ' ' && syllables[accents[0]-1][0] == ' ') linesWithMonoSyllableImmediatelyPrecedingFinalAccent.push(line);
+  if(syllables.length < 2 || accents.length < 2) console.info(line);
+  return accents;
 };
-q()
+
+var totalLines = 0;
+for(i in novaVulgata) {
+  var line = novaVulgata[i].trim();
+  if(line.match(/^PSALMUS\s/)) continue;
+  line = line.split(/\s*\*\s*/);
+  q(line[0], regexLatin, regexAccent);
+  q(line[1], regexLatin, regexAccent);
+  ++totalLines
+}
+
+/// *** RGP ***
+// for(var i=1; i <= 150; ++i) {
+//   //console.info(i + '.txt')
+//   var lines = fs.readFileSync('../Psalms-RGP/rgp-pointed/' + i + '.txt', {encoding:'UTF8'}).split(/\s*\n\s*/);
+//   for(j in lines) {
+//     var line = lines[j].trim();
+//     if(!line) continue;
+//     if(q(line, regexEnglish, regexEnglishAccent)) ++totalLines;
+//   }
+// }
+
+
+
+
+console.info('lines with mono syllable immediately preceding final accent:');
+console.info(linesWithMonoSyllableImmediatelyPrecedingFinalAccent.join('\n'));
+console.info({totalLines, linesWithThreeOrMoreSyllablesBetweenFinalAccents, linesWithMonoSyllableImmediatelyPrecedingFinalAccent: linesWithMonoSyllableImmediatelyPrecedingFinalAccent.length});
