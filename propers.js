f8cef1268b24ca4a51ea80e220ffb463ad074e18
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-10-04 08:25:58 -0400

    Always allow verses to be further split up when psalm toning a verse of 50 syllables or more

--- propers.js
+++ propers.js
@@ -984,7 +984,7 @@ $(function(){
       // make the rest of the first word minuscule
       s=s[0] + s.slice(1,index).toLowerCase() + s.slice(index);
     }
-    s = s.replace(/\s*~\s*/g,'\n').replace(/%/g,'*').replace(/\s*\|\s*\n/g,'\n').replace(/(\|\s*)*(\*\s*)+(\|\s*)*/g,'* ').replace(/\s*[*|]?\s*$/,'');
+    s = s.replace(/\s*~\s*/g,'\n').replace(/%/g,'*').replace(/\s*\|\s*\n/g,'\n').replace(/(\|\s*)*(\*\s*)+(\|\s*)*/g,'* ').replace(/(\|\s*)+/g,'| ').replace(/\s*[*|]?\s*$/,'');
     return s;
   };
   
@@ -1035,7 +1035,7 @@ $(function(){
     return makePattern(line);
   }
   var versify = function(line, allowSplitting){
-    if(allowSplitting) return splitIntoVerses(line);
+    if(allowSplitting || line.countSyllables() >= 50) return splitIntoVerses(line);
     return makePattern(line);
   }
 
