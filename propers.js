a230c184541646bb1923399848619854d0a14d79
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2018-05-16 11:13:24 -0400

    Properly remove ij. from 5th alleluia on Pentecost Saturday

--- propers.js
+++ propers.js
@@ -423,7 +423,7 @@ $(function(){
             if(part=='graduale' || (part=='alleluia' && !gradualeIsFirstAlleluia)) {
               // add ij. if not present:
               gabc = gabc.replace(/(al\([^)]+\)le\([^)]+\)l[uú]\([^)]+\)[ij]a[.,;:](?:\s+\*|\([^)]+\)\W+\*))(?!(\([^)]+\)\s*)*\s*(<i>)?ij\.?(<\/i>)?)(?!(?:\([,;:]\)|\s+|<i>|\()*non\s+rep[eé]titur)/i,'$1 <i>ij.</i>');
-            } else if(part=='alleluia' && gradualeIsFirstAlleluia) {
+            } else if((part=='alleluia' && gradualeIsFirstAlleluia) || /^graduale[1-9]/.test(part)) {
               // remove ij. if present
               gabc = gabc.replace(/(al\([^)]+\)le\([^)]+\)l[uú]\([^)]+\)[ij]a[.,;:](?:\s+\*)?\([^)]+\)\W+)(<i>)?ij\.?(<\/i>)?/i,'$1');
             }
