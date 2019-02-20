fcc78eb5a1e9e0004168ab183a8bb623704e03ae
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2019-02-20 12:41:37 -0500

    Updated cordova android library and moved some cordova things around

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export ORG_GRADLE_PROJECT_cdvMinSdkVersion=21
+#export ORG_GRADLE_PROJECT_cdvReleaseSigningPropertiesFile=../../../key.properties
 cordova build --release --device
 /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
