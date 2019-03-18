6823b0cffba32664b047d65aac133a1d02c641c4
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2019-03-17 20:29:36 -0400

    Fixed a problem reading references during lent

--- propers.js
+++ propers.js
@@ -718,7 +718,27 @@ $(function(){
       readings = [readings[0]].concat(readings.slice(-2));
     }
     var ref = proprium[selDay] && proprium[selDay].ref || selDay;
-    selPropers = proprium[selDay + selTempus] || proprium[ref + selTempus] || proprium[ref];
+    selPropers = proprium[selDay + selTempus] || proprium[ref + selTempus];
+    if(!selPropers && proprium[ref]) {
+      selPropers = proprium[ref];
+      if(selTempus && (selDay != ref)) {
+        selPropers = $.extend({},selPropers);
+        var regex;
+        if(selTempus == 'Quad') {
+          delete selPropers.alID;
+          regex = /^(\w{2,3})(?:Quad|Sept)ID$/;
+        } else if(selTempus == 'Pasch') {
+          selPropers.grID = selPropers.alID;
+          regex = /^(\w{2,3})PaschID$/;
+        }
+        Object.keys(selPropers).forEach(function(key) {
+          var match = regex.exec(key);
+          if(match) {
+            selPropers[match[1]+"ID"] = selPropers[key];
+          }
+        })
+      }
+    }
     if(selPropers && selPropers.ref) selPropers = proprium[selPropers.ref];
     if(selPropers && /^(?:Adv|Quad|[765]a)/.test(selDay) && !('gloria' in selPropers)) {
       selPropers.gloria = false;
