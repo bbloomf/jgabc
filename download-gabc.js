2ea7fbb87650505f75bd10689604c2cf59b0b3eb
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2019-03-25 09:35:45 -0400

    Updated vulgate Added references to Vidi Aquam and Asperges

--- download-gabc.js
+++ download-gabc.js
@@ -8,6 +8,9 @@ gabcRefs["2060"] = "Ex 15: 1-2";
 gabcRefs["2075"] = "Is 5: 1-2";
 gabcRefs["2086"] = "Deut 32: 1-4";
 gabcRefs["943"] = "Ps 41: 2-4";
+gabcRefs["958"] = "Ez 47: 2, 9; Ps 117: 1";
+gabcRefs["497"] = "Ps 50: 9, 3a";
+gabcRefs["905"] = "Ps 50: 9, 3a";
 
 window = {Hypher: Hypher};
 Hypher.languages = {};
