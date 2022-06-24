d7f6d85c78894e3bf62bc2f69a2cfaed2e53cff5
Benjamin Bloomfield <bhb123@gmail.com>
2022-06-24 11:07:31 -0400

    Launch apk directory after completing build

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Hom
 cordova build --release --device
 # /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
 /Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m upload -assetFile platforms/ios/build/device/Chant\ Tools.ipa -u bhb123@gmail.com -p @file:appid-secret.key
+open platforms/android/app/build/outputs/apk/release
