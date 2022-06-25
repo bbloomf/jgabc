24eb3c2b13725df852ee6bdaeb647b1fe0dc8503
Benjamin Bloomfield <bhb123@gmail.com>
2022-06-25 05:33:28 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cordova build --release --device
 # /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
 /Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m upload -assetFile platforms/ios/build/device/Chant\ Tools.ipa -u bhb123@gmail.com -p @file:appid-secret.key
 open platforms/android/app/build/outputs/apk/release
+sh open-new-release.sh
