0c10c9658c8e7f550cee5bc3f0ec901942b6f3b5
Benjamin Bloomfield <bhb123@gmail.com>
2023-03-08 11:30:04 -0500

    Add tract and easter time 2nd alleluia for votive Mass of the Immaculate Heart

--- propersdata.js
+++ propersdata.js
@@ -201,6 +201,7 @@ var otherKeys = [
     {key:"SMlent",title:"A Purificatione usque ad Pascha",en:"From Candlemas to Easter"},
     {key:"SMeaster",title:"A Pascha usque ad Pentecosten",en:"From Easter to Pentecost"},
     {key:"SMpentecost",title:"A Pentecoste usque ad Adventum",en:"From Pentecost to Advent"},
+    {key:"Aug22",title:"Immaculati Cordis B.M.V.",en:"Of the Immaculate Heart of BVM"},
     {group:true, title:"Missæ votivæ aliæ",en:"Other Votive Masses"},
     {key:"votiveESP", title:"Pro eligendo Summo Pontifice",en:"For electing a pope"},
     {key:"votiveFP", title:"Pro Fidei Propagatione",en:"For the propagation of the faith"},
@@ -4114,6 +4115,24 @@ var proprium = {
         "coID": 982,
         "grID": 778,
         "alID": 881,
+        "inID": 1110,
+        "ofID": 1045,
+        "gbid": "immaculate_heart_bvm",
+        "coVerses": "Luc 1: 46-55"
+    },
+    "Aug22Quad": {
+        "coID": 982,
+        "grID": 778,
+        "trID": 793,
+        "inID": 1110,
+        "ofID": 1045,
+        "gbid": "immaculate_heart_bvm",
+        "coVerses": "Luc 1: 46-55"
+    },
+    "Aug22Pasch": {
+        "coID": 982,
+        "grID": 881,
+        "alID": 460,
         "inID": 1110,
         "ofID": 1045,
         "gbid": "immaculate_heart_bvm",
