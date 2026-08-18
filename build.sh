384fe2beb73b7cc6974d38fef1a6ae49d6c2437d
Benjamin Bloomfield <bhb123@gmail.com>
2026-08-18 17:13:09 -0400

    chore: Update ios config

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 export ORG_GRADLE_PROJECT_cdvMinSdkVersion=21
 export ANDROID_HOME=~/Library/Android/sdk
-PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$ANDROID_HOME/emulator
+PATH="/usr/bin:$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$ANDROID_HOME/emulator"
 # export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home
 # export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
 #export ORG_GRADLE_PROJECT_cdvReleaseSigningPropertiesFile=../../../key.properties
-cordova build --release --device
-# /Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool --upload-app -f platforms/ios/build/device/Chant\ Tools.ipa -u bhb.123@gmail.com
-/Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m upload -assetFile platforms/ios/build/device/Chant\ Tools.ipa -u bhb123@gmail.com -p @file:appid-secret.key
+cordova build --release --device ios
+ALTOOL_PW="$(cat appid-secret.key)" xcrun altool --upload-app --show-progress -f platforms/ios/build/Release-iphoneos/Chant\ Tools.ipa -u bhb123@gmail.com -p @env:ALTOOL_PW
 open platforms/android/app/build/outputs/apk/release
 sh open-new-release.sh
